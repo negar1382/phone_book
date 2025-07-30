9a36b41bbf5a83c89352ca28f8bcaff4bfccb1d2
negar1382 <almasss7913@gmail.com>
2025-07-30 20:00:24 +0330

    Update views.py

--- phone/views.py
+++ phone/views.py
@@ -12,6 +12,8 @@ def index(request, department=None):
     print(query)
     if department:
         users = Users.objects.filter(department=department)
+        if query:
+            users = Users.objects.filter(name__icontains=query)
 
     elif query:
         users = Users.objects.filter(name__icontains=query)
