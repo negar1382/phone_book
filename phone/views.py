8ef5b2fdbfcdd61eaa09b83fde53ca47b8a91ff0
negar1382 <almasss7913@gmail.com>
2025-08-01 12:29:05 +0330

    Update style, template, and view for responsive table and cards

--- phone/views.py
+++ phone/views.py
@@ -11,14 +11,14 @@ def index(request, department=None):
     query = request.GET.get('query')
     print(query)
     if department:
-        users = Users.objects.filter(department=department)
+        users = Users.objects.filter(department=department).order_by('name')
         if query:
-            users = Users.objects.filter(name__icontains=query)
+            users = Users.objects.filter(name__icontains=query).order_by('name')
 
     elif query:
-        users = Users.objects.filter(name__icontains=query)
+        users = Users.objects.filter(name__icontains=query).order_by('name')
     else:
-        users = Users.objects.all()
+        users = Users.objects.all().order_by('name')
 
     return render(request, "phone/info.html", {'user_info': users})
 
